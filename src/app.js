de52a922d086fd7a11a2f9be1bf28845f2c3ed08
unknown <chez90@mail.ru>
2019-08-07 20:25:18 +0400

    first fixing, using eslint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-import './scss/base.scss'
-import {elem, createTable} from './js/table.js'
+import './scss/base.scss';
+import { elem, createTable } from './js/table.js';
 
 createTable(elem, 20, 10);
-
