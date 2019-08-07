f032241f1256ac4a933d3c104ef4faa4f0036cdb
unknown <chez90@mail.ru>
2019-08-07 18:44:07 +0400

    install HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -19,7 +20,10 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new ExtractTextPlugin('style.css')
+		new ExtractTextPlugin('style.css'),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
 	],
 	devServer: {
 		port: 8080
