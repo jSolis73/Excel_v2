54e3d93cfeccafe913d267e36d8ae5fecdc8cba1
unknown <chez90@mail.ru>
2019-08-09 00:40:48 +0400

    remake the table using div blocks

--- src/js/table.js
+++ src/js/table.js
@@ -1,19 +1,23 @@
 export const elem = document.querySelector('#table');
 
-export function createTable(parent, row, column) {
-  const table = document.createElement('table');
-
-  for (let i = 0; i < row; i++) {
-    const tr = document.createElement('tr');
-
-    for (let j = 0; j < column; j++) {
-      const td = document.createElement('td');
-      const input = document.createElement('input');
-      input.classList.add('ceil');
-      td.appendChild(input);
-      tr.appendChild(td);
+export function createTable(parent, rowCount, ceilCount) {
+  let trTh = '<div class="tr">';
+  for (let i = 0; i < ceilCount + 1; i++) {
+    const arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    if (i === 0) {
+      trTh += '<div class="trTh"></div>';
+    } else {
+      trTh += `<div class="tdTh">${arr[i - 1]}</div>`;
     }
-    table.appendChild(tr);
   }
-  parent.appendChild(table);
+  trTh += '</div>';
+  elem.innerHTML += trTh; 
+  for (let j = 0; j < rowCount; j++) {
+    let tr = `<div class="tr"><div class="trTh">${j + 1}</div>`;
+    for (let k = 0; k < ceilCount; k++) {
+      tr += '<div class="td" contenteditable="true"></div>';
+    }
+    tr += '</div>';
+    elem.innerHTML += tr;
+  }
 }
