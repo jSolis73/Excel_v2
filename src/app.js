74a57dd2423346ecaf0a6dd7194b99601a409f30
unknown <chez90@mail.ru>
2019-08-15 13:04:10 +0400

    correct resize method

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './scss/base.scss';
 import createTable from './js/table';
-import resizeCells from './js/divResizing';
+import resizeCells from './js/cellResizing';
 
 createTable('#table', 20, 10);
 resizeCells();
--- src/js/cellResizing.js
+++ src/js/cellResizing.js
@@ -1,6 +1,7 @@
 export default function resizeCells() {
   document.onmousedown = function clickDown(e) {
     const div = e.target.parentNode;
+    const divValue = div.textContent;
     const currentWidth = div.offsetWidth;
     const shiftX = e.clientX;
     if (e.target.className === 'resizeX') {
@@ -9,6 +10,13 @@ export default function resizeCells() {
         const shiftX2 = event.clientX;
         const newWidth = shiftX2 - shiftX;
         div.style.width = `${currentWidth + newWidth}px`;
+        const rows = document.getElementsByClassName('tr');
+        for (let i = 1; i < rows.length; i++) {
+          function resize() {
+            document.getElementById(`${divValue + i}`).style.width = div.style.width;
+          }
+          resize();
+        }
       }
       function onMouseUp() {
         div.parentNode.style.cursor = 'default';
