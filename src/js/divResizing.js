d9d6eb08b4fd8b7019d9533e1f3d78ecfbdcfb9b
unknown <chez90@mail.ru>
2019-08-15 11:09:19 +0400

    correct eslint

--- src/js/divResizing.js
+++ src/js/divResizing.js
@@ -1,22 +1,22 @@
 export default function resizeDiv() {
-  document.onmousedown = function (e) {
-		if (e.which == 1 && e.target.className === 'resizeX') {
-			const div = e.target.parentNode;
-			const currentWidth = div.offsetWidth;
-			const shiftX = e.clientX;
-			document.addEventListener('mousemove', onMouseMove);
-			document.addEventListener('mouseup', onMouseUp);
-			function onMouseMove(e) {
-				div.parentNode.style.cursor = 'col-resize';
-				const shiftX2 = e.clientX;
-				const newWidth = shiftX2 - shiftX;
-				div.style.width = `${currentWidth + newWidth}px`;
-			}
-			function onMouseUp() {
-				div.parentNode.style.cursor = 'default';
-				document.removeEventListener('mousemove', onMouseMove);
-				document.removeEventListener('mouseup', onMouseUp);
-			}
-		}
-  }
+  document.onmousedown = function clickDown(e) {
+    const div = e.target.parentNode;
+    const currentWidth = div.offsetWidth;
+    const shiftX = e.clientX;
+    if (e.target.className === 'resizeX') {
+      function onMouseMove(event) {
+        div.parentNode.style.cursor = 'col-resize';
+        const shiftX2 = event.clientX;
+        const newWidth = shiftX2 - shiftX;
+        div.style.width = `${currentWidth + newWidth}px`;
+      }
+      function onMouseUp() {
+        div.parentNode.style.cursor = 'default';
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+  };
 }
