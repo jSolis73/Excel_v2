0f4d2ab0a72e4c76efa5621e47140c621a84ea02
unknown <chez90@mail.ru>
2019-08-18 12:22:54 +0400

    local storage

--- src/js/cellResizing.js
+++ src/js/cellResizing.js
@@ -1,9 +1,11 @@
 export default function resizeCells() {
-  const data = {
+  let data = {
     tdTh: {},
     td: {},
     tr: {},
   };
+  const stored = localStorage.getItem('data');
+  if (stored) data = JSON.parse(stored);
   document.onmousedown = function clickDown(e) {
     const div = e.target.parentNode;
     const divValue = div.textContent;
@@ -11,8 +13,8 @@ export default function resizeCells() {
     const currentHeight = div.offsetHeight;
     const firstPointX = e.clientX;
     const firstPointY = e.clientY;
-    if (e.target.className === 'table__tr__td-th__resizeX') {
-      function onMouseMove(event) {
+    function onMouseMove(event) {
+      if (e.target.className === 'table__tr__td-th__resizeX') {
         div.parentNode.style.cursor = 'col-resize';
         const secondPointX = event.clientX;
         const newWidth = secondPointX - firstPointX;
@@ -26,30 +28,22 @@ export default function resizeCells() {
           }
           resize();
         }
-      }
-      function onMouseUp() {
-        div.parentNode.style.cursor = 'default';
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      }
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-    } else if (e.target.className === 'table__tr__tr-th__resizeY') {
-      function onMouseMove(event) {
+      } else if (e.target.className === 'table__tr__tr-th__resizeY') {
         div.style.cursor = 'row-resize';
+        div.style.cursor = 'default';
         const secondPointY = event.clientY;
         const newHeight = secondPointY - firstPointY;
         div.parentNode.style.height = `${currentHeight + newHeight}px`;
         data.tr[`${div.parentNode.id}`] = `${currentHeight + newHeight}px`;
-        localStorage.setItem('storage', JSON.stringify(data));
       }
-      function onMouseUp() {
-        div.style.cursor = 'default';
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      }
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
+      localStorage.setItem('storage', JSON.stringify(data));
     }
+    document.addEventListener('mousemove', onMouseMove);
+    function onMouseUp() {
+      div.parentNode.style.cursor = 'default';
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+    document.addEventListener('mouseup', onMouseUp);
   };
 }
