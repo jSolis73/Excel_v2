63780159c650d1bbbe31ee61c3f2b60f02b154d6
unknown <chez90@mail.ru>
2019-08-28 20:38:15 +0400

    correcting on lesson

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,4 +1,4 @@
-export function getState(name, key, defaultValue) {
+export function getState(name, key, defaultValue = '') {
   const state = localStorage.getItem(name)
     ? JSON.parse(localStorage.getItem(name))
     : {};
@@ -12,20 +12,4 @@ export function saveState(name, key, value) {
     : {};
   state[key] = value;
   localStorage.setItem(name, JSON.stringify(state));
-}
-
-export function saveText(name, id, value) {
-  const state = localStorage.getItem(name)
-    ? JSON.parse(localStorage.getItem(name))
-    : {};
-  state[id] = value;
-  localStorage.setItem(name, JSON.stringify(state));
-}
-
-export function getText(name, id) {
-  const state = localStorage.getItem(name)
-    ? JSON.parse(localStorage.getItem(name))
-    : {};
-
-  return state[id] ? state[id] : '';
 }
--- src/js/table.js
+++ src/js/table.js
@@ -1,4 +1,4 @@
-import { getState, getText } from './localStorage';
+import { getState } from './localStorage';
 
 const DEFAULT_HEIGHT = 33;
 const DEFAULT_WIDTH = 120;
@@ -48,7 +48,7 @@ export default function createTable(parent, rowCount, cellCount) {
     for (let k = 0; k < cellCount; k++) {
       const col = String.fromCharCode('A'.charCodeAt(0) + k);
       const width = `${getState('col-state', col, DEFAULT_WIDTH)}px`;
-      const text = `${getText('text-state', col + j)}`;
+      const text = `${getState('text-state', col + j)}`;
       body += `
         <div
           class="table__td"
@@ -56,7 +56,7 @@ export default function createTable(parent, rowCount, cellCount) {
           data-col="${col}"
           id="${col + j}"
           style="width: ${width}"
-          data-text="text"
+          data-type="text"
         >
           ${text}
         </div>
