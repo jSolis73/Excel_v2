4a735f39ab1199a166cd03dad687b54de6f5c231
unknown <chez90@mail.ru>
2019-09-01 16:09:03 +0400

    rename buttones`s module

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import './scss/base.scss';
 import createTable from './js/table';
 import resizeCells from './js/cellResizing';
 import saveContent from './js/textSaving';
-import createIcons from './js/icons';
+import createIcons from './js/alignButtons';
 import { selectCells } from './js/selectingCells';
 import toAlign from './js/formattingText';
 
